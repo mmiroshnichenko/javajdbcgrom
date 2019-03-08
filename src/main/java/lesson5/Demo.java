3831e6011dd0389f24a925f6cce7813d594d3de6
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-03-08 20:52:56 +0200

    lesson5

---
+++ src/main/java/lesson5/Demo.java
@@ -0,0 +1,39 @@
+package lesson5;
+
+import org.hibernate.Session;
+
+public class Demo {
+    public static void main(String[] args) {
+//        Session session = new HibernateUtils().createSessionFactory().openSession();
+//
+//        session.getTransaction().begin();
+//
+//        Product product = new Product();
+//        product.setId(99);
+//        product.setName("table");
+//        product.setDescription("grey & blue");
+//        product.setPrice(70);
+//
+//        session.save(product);
+//
+//        session.getTransaction().commit();
+//
+//        System.out.println("Done");
+//
+//        session.close();
+
+        Product product1 = new Product();
+        product1.setId(201);
+        product1.setName("testName1");
+        product1.setDescription("testDescription1");
+        product1.setPrice(100);
+
+        //ProductRepository.save(product1);
+
+        product1.setPrice(200);
+        ProductRepository.update(product1);
+
+        ProductRepository.delete(201);
+
+    }
+}
