e69470e5be24e9fda8a619bf3d7a20ef466bc9fc
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-03-13 20:47:08 +0200

    lesson2.hw2

--- src/main/java/hibernate/lesson2/hw2/ProductDAO.java
+++ src/main/java/hibernate/lesson2/hw2/ProductDAO.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.NativeQuery;
-import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -14,222 +13,152 @@ public class ProductDAO {
     private static SessionFactory sessionFactory;
 
     public static Product findById(long id) {
-        Session session = null;
-        Transaction tr = null;
-        Product product = null;
-        try {
-            session = createSessionFactory().openSession();
-            tr = session.getTransaction();
+        try (Session session = createSessionFactory().openSession()) {
+            Transaction tr = session.getTransaction();
             tr.begin();
 
             NativeQuery query = session.createNativeQuery("SELECT * FROM PRODUCT WHERE ID = :id");
             query.addEntity(Product.class);
             query.setParameter("id", id);
-            product = (Product) query.getSingleResult();
+            Product product = (Product) query.getSingleResult();
 
             tr.commit();
+
+            return product;
         } catch (HibernateException e) {
             System.err.println("Save is failed");
             System.err.println(e.getMessage());
-
-            if (tr != null) {
-                tr.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
 
-        return product;
+        return null;
     }
 
     public static List<Product> findByName(String name) {
-        Session session = null;
-        Transaction tr = null;
-        List<Product> products = null;
-        try {
-            session = createSessionFactory().openSession();
-            tr = session.getTransaction();
+        try (Session session = createSessionFactory().openSession()) {
+            Transaction tr = session.getTransaction();
             tr.begin();
 
             NativeQuery query = session.createNativeQuery("SELECT * FROM PRODUCT WHERE NAME = :name");
             query.addEntity(Product.class);
             query.setParameter("name", name);
-            products = query.list();
+            List<Product> products = query.list();
 
             tr.commit();
+
+            return products;
         } catch (HibernateException e) {
             System.err.println("Save is failed");
             System.err.println(e.getMessage());
-
-            if (tr != null) {
-                tr.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
 
-        return products;
+        return null;
     }
 
     public static List<Product> findByContainedName(String name) {
-        Session session = null;
-        Transaction tr = null;
-        List<Product> products = null;
-        try {
-            session = createSessionFactory().openSession();
-            tr = session.getTransaction();
+        try (Session session = createSessionFactory().openSession()) {
+            Transaction tr = session.getTransaction();
             tr.begin();
 
             NativeQuery query = session.createNativeQuery("SELECT * FROM PRODUCT WHERE NAME like :name");
             query.addEntity(Product.class);
             query.setParameter("name", "%" + name + "%");
-            products = query.list();
+            List<Product> products = query.list();
 
             tr.commit();
+
+            return products;
         } catch (HibernateException e) {
             System.err.println("Save is failed");
             System.err.println(e.getMessage());
-
-            if (tr != null) {
-                tr.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
 
-        return products;
+        return null;
     }
 
     public static List<Product> findByPrice(int price, int delta) {
-        Session session = null;
-        Transaction tr = null;
-        List<Product> products = null;
-        try {
-            session = createSessionFactory().openSession();
-            tr = session.getTransaction();
+        try (Session session = createSessionFactory().openSession()) {
+            Transaction tr = session.getTransaction();
             tr.begin();
 
             NativeQuery query = session.createNativeQuery("SELECT * FROM PRODUCT WHERE PRICE BETWEEN :minPrice AND :maxPrice");
             query.addEntity(Product.class);
             query.setParameter("minPrice", price - delta);
             query.setParameter("maxPrice", price + delta);
-            products = query.list();
+            List<Product> products = query.list();
 
             tr.commit();
+
+            return products;
         } catch (HibernateException e) {
             System.err.println("Save is failed");
             System.err.println(e.getMessage());
-
-            if (tr != null) {
-                tr.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
 
-        return products;
+        return null;
     }
 
     public static List<Product> findByNameSortedAsc(String name) {
-        Session session = null;
-        Transaction tr = null;
-        List<Product> products = null;
-        try {
-            session = createSessionFactory().openSession();
-            tr = session.getTransaction();
+        try (Session session = createSessionFactory().openSession()) {
+            Transaction tr = session.getTransaction();
             tr.begin();
 
             NativeQuery query = session.createNativeQuery("SELECT * FROM PRODUCT WHERE NAME like :name ORDER BY NAME ASC");
             query.addEntity(Product.class);
             query.setParameter("name", "%" + name + "%");
-            products = query.list();
+            List<Product> products = query.list();
 
             tr.commit();
+
+            return products;
         } catch (HibernateException e) {
             System.err.println("Save is failed");
             System.err.println(e.getMessage());
-
-            if (tr != null) {
-                tr.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
 
-        return products;
+        return null;
     }
 
     public static List<Product> findByNameSortedDesc(String name) {
-        Session session = null;
-        Transaction tr = null;
-        List<Product> products = null;
-        try {
-            session = createSessionFactory().openSession();
-            tr = session.getTransaction();
+        try (Session session = createSessionFactory().openSession()) {
+            Transaction tr = session.getTransaction();
             tr.begin();
 
             NativeQuery query = session.createNativeQuery("SELECT * FROM PRODUCT WHERE NAME like :name ORDER BY NAME DESC");
             query.addEntity(Product.class);
             query.setParameter("name", "%" + name + "%");
-            products = query.list();
+            List<Product> products = query.list();
 
             tr.commit();
+
+            return products;
         } catch (HibernateException e) {
             System.err.println("Save is failed");
             System.err.println(e.getMessage());
-
-            if (tr != null) {
-                tr.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
 
-        return products;
+        return null;
     }
 
     public static List<Product> findByPriceSortedDesc(int price, int delta) {
-        Session session = null;
-        Transaction tr = null;
-        List<Product> products = null;
-        try {
-            session = createSessionFactory().openSession();
-            tr = session.getTransaction();
+        try (Session session = createSessionFactory().openSession()) {
+            Transaction tr = session.getTransaction();
             tr.begin();
 
             NativeQuery query = session.createNativeQuery("SELECT * FROM PRODUCT WHERE PRICE BETWEEN :minPrice AND :maxPrice ORDER BY PRICE DESC");
             query.addEntity(Product.class);
             query.setParameter("minPrice", price - delta);
             query.setParameter("maxPrice", price + delta);
-            products = query.list();
+            List<Product> products = query.list();
 
             tr.commit();
+
+            return products;
         } catch (HibernateException e) {
             System.err.println("Save is failed");
             System.err.println(e.getMessage());
-
-            if (tr != null) {
-                tr.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
 
-        return products;
+        return null;
     }
 
     public static SessionFactory createSessionFactory() {
