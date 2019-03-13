8c212423c9fbe0ff0e1dec44560aa82e2846e643
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-03-13 21:30:43 +0200

    lesson4.hw

--- src/main/java/jdbc/lesson4/hw1/dao/FileDAO.java
+++ src/main/java/jdbc/lesson4/hw1/dao/FileDAO.java
@@ -3,63 +3,22 @@ package jdbc.lesson4.hw1.dao;
 import jdbc.lesson4.hw1.model.File;
 import jdbc.lesson4.hw1.model.Storage;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileDAO extends BaseDAO<File> {
+public class FileDAO {
+    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.ckzanzkzkznq.us-east-2.rds.amazonaws.com:1521:ORCL";
+
+    private static final String USER = "main";
+    private static final String PASS = "W1DdNDwypsUyohttCttN";
+
     StorageDAO storageDAO = new StorageDAO();
 
-    public List<File>getFilesByStorage(Storage storage) throws Exception {
-        List<File> files = new ArrayList<>();
-        try(Connection connection = getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM FILES WHERE STORAGE_ID = ?")) {
-            preparedStatement.setLong(1, storage.getId());
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                File file = new File(resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getLong(4),
-                        storage);
-                files.add(file);
-            }
-
-            return files;
-        } catch (SQLException e) {
-            System.err.println("Something went wrong");
-            e.printStackTrace();
-        }
-
-        return files;
-    }
-
-    public void updateFilesList(List<File> files) throws SQLException {
-        try (Connection connection = getConnection()) {
-            updateObjectListInDb(connection, files);
-        } catch (SQLException e) {
-            throw e;
-        }
-    }
-
-    @Override
-    protected String getTableName() {
-        return "FILES";
-    }
-
-    @Override
-    protected String getObjectName() {
-        return "File";
-    }
-
-    @Override
-    protected File addObjectInDb(Connection connection, File file) throws SQLException {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO FILES VALUES (?, ?, ?, ?, ?)")) {
-
+    public File save(File file) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO FILES VALUES (?, ?, ?, ?, ?)"))
+        {
             file.setId(getNextId());
             preparedStatement.setLong(1, file.getId());
             preparedStatement.setString(2, file.getName());
@@ -71,23 +30,97 @@ public class FileDAO extends BaseDAO<File> {
                 preparedStatement.setString(5, null);
             }
             preparedStatement.executeUpdate();
+
+            return file;
         } catch (SQLException e) {
             throw new SQLException("Error: file: " + file.getName() + "." + file.getFormat() + " has not saved");
         }
-
-        return file;
     }
 
-    @Override
-    protected File updateObjectInDb(Connection connection, File file) throws SQLException {
-        List<File> files = new ArrayList<>();
-        files.add(file);
-        updateObjectListInDb(connection, files);
+    public File update(File file) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("UPDATE FILES SET NAME = ?, FORMAT = ?, FILE_SIZE = ?, STORAGE_ID = ? WHERE ID = ?"))
+        {
+            preparedStatement.setString(1, file.getName());
+            preparedStatement.setString(2, file.getFormat());
+            preparedStatement.setLong(3, file.getSize());
+            if (file.getStorage() != null) {
+                preparedStatement.setLong(4, file.getStorage().getId());
+            } else {
+                preparedStatement.setString(4, null);
+            }
+            preparedStatement.setLong(5, file.getId());
+            preparedStatement.executeUpdate();
 
-        return file;
+            return file;
+        } catch (SQLException e) {
+            throw new SQLException("Error: file: " + file.getName() + "." + file.getFormat() + " has not updated");
+        }
     }
 
-    private void updateObjectListInDb(Connection connection, List<File> files) throws SQLException {
+    public void delete(long id) throws SQLException {
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM FILES WHERE ID = ?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException("Error: File(id: " + id + ") has not deleted");
+        }
+    }
+
+    public File findById(long id) throws SQLException {
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM FILES WHERE ID = ?")) {
+            preparedStatement.setLong(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                return new File(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getLong(4),
+                        resultSet.getString(5) != null ? storageDAO.findById(resultSet.getLong(5)) : null);
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Error: File(id: " + id + ") has not found");
+        }
+
+        return null;
+    }
+
+    public List<File>getFilesByStorage(Storage storage) throws SQLException {
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM FILES WHERE STORAGE_ID = ?"))
+        {
+            preparedStatement.setLong(1, storage.getId());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<File> files = new ArrayList<>();
+            while (resultSet.next()) {
+                File file = new File(resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getLong(4),
+                        storage);
+                files.add(file);
+            }
+
+            return files;
+        } catch (SQLException e) {
+            throw new SQLException("Error: Something went wrong. Can't get files by storage(id:" + storage.getId());
+        }
+    }
+
+    public void updateFilesList(List<File> files) throws SQLException {
+        try (Connection connection = getConnection()) {
+            updateList(connection, files);
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    private void updateList(Connection connection, List<File> files) throws SQLException {
         long fileId = 0;
         String fileName = "";
         String fileFormat = "";
@@ -118,18 +151,22 @@ public class FileDAO extends BaseDAO<File> {
         }
     }
 
-    @Override
-    protected File mapObjectFromDb(ResultSet resultSet) throws SQLException {
-        Storage storage = null;
-        if (resultSet.getString(5) != null) {
-            storage = storageDAO.findById(resultSet.getLong(5));
+    private long getNextId() throws SQLException {
+        long id = 1;
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT MAX(ID) FROM FILES");
+            while (resultSet.next()) {
+                id += resultSet.getLong(1);
+            }
+
+        } catch (SQLException e) {
+            throw e;
         }
 
-        return new File(
-                resultSet.getLong(1),
-                resultSet.getString(2),
-                resultSet.getString(3),
-                resultSet.getLong(4),
-                storage);
+        return id;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
     }
 }
--- src/main/java/jdbc/lesson4/hw1/dao/StorageDAO.java
+++ src/main/java/jdbc/lesson4/hw1/dao/StorageDAO.java
@@ -2,26 +2,18 @@ package jdbc.lesson4.hw1.dao;
 
 import jdbc.lesson4.hw1.model.Storage;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class StorageDAO extends BaseDAO<Storage> {
-    @Override
-    protected String getTableName() {
-        return "STORAGE";
-    }
+public class StorageDAO{
+    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.ckzanzkzkznq.us-east-2.rds.amazonaws.com:1521:ORCL";
 
-    @Override
-    protected String getObjectName() {
-        return "Storage";
-    }
+    private static final String USER = "main";
+    private static final String PASS = "W1DdNDwypsUyohttCttN";
 
-    @Override
-    protected Storage addObjectInDb(Connection connection, Storage storage) throws SQLException {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO STORAGE VALUES (?, ?, ?, ?)")) {
-
+    public Storage save(Storage storage) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO STORAGE VALUES (?, ?, ?, ?)"))
+        {
             storage.setId(getNextId());
             preparedStatement.setLong(1, storage.getId());
             preparedStatement.setString(2, String.join(",", storage.getFormatsSupported()));
@@ -30,34 +22,72 @@ public class StorageDAO extends BaseDAO<Storage> {
 
             preparedStatement.executeUpdate();
 
+            return storage;
         } catch (SQLException e) {
             throw new SQLException("Error: new storage has not saved");
         }
-
-        return storage;
     }
 
-    @Override
-    protected Storage updateObjectInDb(Connection connection, Storage storage) throws SQLException {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE STORAGE SET FORMATS_SUPPORTED = ?, STORAGE_COUNTRY = ?, STORAGE_MAX_SIZE = ? WHERE ID = ?")) {
+    public Storage update(Storage storage) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("UPDATE STORAGE SET FORMATS_SUPPORTED = ?, STORAGE_COUNTRY = ?, STORAGE_MAX_SIZE = ? WHERE ID = ?"))
+        {
             preparedStatement.setString(1, String.join(",", storage.getFormatsSupported()));
             preparedStatement.setString(2, storage.getStorageCountry());
             preparedStatement.setLong(3, storage.getStorageMaxSize());
             preparedStatement.setLong(4, storage.getId());
 
-            preparedStatement.executeUpdate();
+            return storage;
         } catch (SQLException e) {
             throw new SQLException("Error: storage(id: " + storage.getId() + ") has not updated");
         }
-
-        return storage;
     }
 
-    protected Storage mapObjectFromDb(ResultSet resultSet) throws SQLException {
-        return new Storage(
-                resultSet.getLong(1),
-                resultSet.getString(2).split(","),
-                resultSet.getString(3),
-                resultSet.getLong(4));
+    public void delete(long id) throws SQLException {
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM STORAGE WHERE ID = ?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException("Error: Storage(id: " + id + ") has not deleted");
+        }
+    }
+
+    public Storage findById(long id) throws SQLException {
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM STORAGE WHERE ID = ?")) {
+            preparedStatement.setLong(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                return new Storage(
+                        resultSet.getLong(1),
+                        resultSet.getString(2).split(","),
+                        resultSet.getString(3),
+                        resultSet.getLong(4));
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Error: Storage(id: " + id + ") has not found");
+        }
+
+        return null;
+    }
+    private long getNextId() throws SQLException {
+        long id = 1;
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT MAX(ID) FROM STORAGE");
+            while (resultSet.next()) {
+                id += resultSet.getLong(1);
+            }
+
+        } catch (SQLException e) {
+            throw e;
+        }
+
+        return id;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
     }
 }
